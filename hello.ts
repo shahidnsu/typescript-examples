49b5edc1c05d61f29b128d78c01d61908ba5208b
Shahidnsu <shahid.khan@northsouth.edu>
2023-03-05 14:45:14 +0600

    added a basic example

---
+++ hello.ts
@@ -0,0 +1,10 @@
+
+const hello = 'This is just testing'
+let a: string = 'Hello'
+let b: number = 7
+function sayhello(hello: string) {
+  console.log(`${hello}  welcome to selise`)
+
+}
+sayhello('shahid')
+
